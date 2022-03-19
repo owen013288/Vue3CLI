dbf5e368bc5a4eb977a693c0edb74063536bce0a
owen <owen013288@gmail.com>
2022-03-19 22:17:57 +0800

    刪除不必要的HelloWorld、APP裡的資料 更改vue.config.js 裡面的命名規則，讓我的命名可以順利 並新增V1.vue測試

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <V1 />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import V1 from "./components/V1";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    V1,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/V1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "V1",
+  data() {
+    return {
+      title: "第一堂課",
+    };
+  },
+};
+</script>
+<style scoped>
+</style>

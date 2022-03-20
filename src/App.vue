225ad81afbfa0815554262730a450e60347ef8f5
owen <owen013288@gmail.com>
2022-03-20 18:04:26 +0800

    補V3出現在APP.vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
   <V1 />
   <hr />
   <V2 />
+  <hr />
+  <V3 />
 </template>
 
 <script>
 import V1 from "./components/V1";
 import V2 from "./components/V2";
+import V3 from "./components/V3";
 
 export default {
   name: "App",
   components: {
     V1,
     V2,
+    V3,
   },
 };
 </script>

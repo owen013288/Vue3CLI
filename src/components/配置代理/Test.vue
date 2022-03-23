05f6a185f9685cdf3b33969ebd534e83da3f042d
owen <owen013288@gmail.com>
2022-03-23 23:20:56 +0800

    安裝axios並在vue.config.js配置代理

---
+++ src/components/配置代理/Test.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="getWeather">獲取天氣信息</button>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Test",
+  data() {
+    return {};
+  },
+  methods: {
+    getWeather() {
+      axios.get("http://localhost:8081/weatherforecast").then(
+        (response) => {
+          // response 響應對象，資料在response.data
+          console.log("成功", response.data);
+        },
+        (error) => {
+          console.log("失敗", error.message);
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  lintOnSave: false //關閉語法檢查
+  lintOnSave: false, //關閉語法檢查
+  //開啟代理服務器 =>　記得重新啟動
+  devServer: {
+    proxy: 'http://localhost:5538'
+  }
 })

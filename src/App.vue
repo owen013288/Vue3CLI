fefd5c001b87700e63c37ed4b3faa9078d1714d2
owen <owen013288@gmail.com>
2022-04-02 14:27:58 +0800

    使用reactive，讓setup函數內的對象可以改變

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <template>
   <h1>一個人的信息</h1>
-  <h2>姓名：{{ name }}</h2>
-  <h2>年齡：{{ age }}</h2>
-  <h2>工作種類：{{ job.type }}</h2>
-  <h2>工作薪水：{{ job.salary }}</h2>
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年齡：{{ person.age }}</h2>
+  <h2>工作種類：{{ person.job.type }}</h2>
+  <h2>工作薪水：{{ person.job.salary }}</h2>
+  <h2>測試數據c： {{ person.job.a.b.c }}</h2>
+  <h2>嗜好： {{ person.hobby }}</h2>
   <button @click="SayHello()">說話(Vue3)</button>
   <button @click="changeInfo()">修改內容(Vue3)</button>
 </template>
 
 <script>
 // import { h } from "vue";
-import { ref } from "vue";
+import { reactive } from "vue";
 export default {
   name: "App",
   setup() {
-    let name = ref("owen");
-    let age = ref(18);
-    let job = ref({
-      type: "前端工程師",
-      salary: "30K",
+    let person = reactive({
+      name: "owen",
+      age: 18,
+      job: {
+        type: "前端工程師",
+        salary: "30K",
+        a: {
+          b: {
+            c: 666,
+          },
+        },
+      },
+      a: {
+        b: {
+          c: 666,
+        },
+      },
+      hobby: ["抽菸", "喝酒", "吃飯"],
     });
 
     function SayHello() {
@@ -26,23 +41,20 @@ export default {
     }
 
     function changeInfo() {
-      name.value = "李四";
-      age.value = 48;
-      job.value.type = "後端工程師";
-      job.value.salary = "50K";
-      console.log(name, age);
+      person.name = "李四";
+      person.age = 48;
+      person.job.type = "後端工程師";
+      person.job.salary = "50K";
+      person.job.a.b.c = 999;
+      person.hobby[0] = "吃飯";
     }
 
     // 返回一個對象
     return {
-      name,
-      age,
+      person,
       SayHello,
       changeInfo,
-      job,
     };
-    // 返回一個渲染函數(了解)
-    // return () => h('h1','owen');
   },
 };
 </script>

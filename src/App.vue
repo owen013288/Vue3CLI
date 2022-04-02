4dad280d17c21fbf81e34dbff02ff4cb923de23e
owen <owen013288@gmail.com>
2022-04-02 12:55:52 +0800

    恢復

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,25 @@
 <template>
-  <div class="container mt-3">
-    <Banner />
-    <div class="row">
-      <div class="col-2">
-        <div class="list-group">
-          <!-- 原始作法 -->
-          <!-- <a href="./about.html" class="list-group-item active">About</a>
-          <a href="./home.html" class="list-group-item">home</a> -->
-
-          <!-- 新作法 -->
-          <router-link
-            :replace="true"
-            class="list-group-item"
-            active-class="active"
-            to="../components/路由/pages/home"
-          >
-            home
-          </router-link>
-          <router-link
-            replace
-            class="list-group-item"
-            active-class="active"
-            :to="{ path: '/about' }"
-          >
-            About
-          </router-link>
-        </div>
-      </div>
-      <div class="col-6">
-        <div class="card">
-          <div class="card-body">
-            <!-- 指定組件的呈現位置 -->
-            <router-view></router-view>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="container">
+    <Search />
+    <List />
   </div>
 </template>
 
 <script>
-import Banner from "./components/路由/Banner.vue";
+import Search from "./components/搜索案例/Search.vue";
+import List from "./components/搜索案例/List.vue";
 export default {
   name: "App",
   components: {
-    Banner,
+    Search,
+    List,
   },
 };
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>

625f40cea5c527e0e842448cc9742c8523f4b1d5
owen <owen013288@gmail.com>
2022-04-02 13:54:26 +0800

    初識vue3的setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <h1>我是APP組件</h1>
+  <h1>一個人的信息</h1>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年齡：{{ age }}</h2>
+  <button @click="SayHello()">說話(Vue3)</button>
 </template>
 
 <script>
+// import { h } from "vue";
 export default {
   name: "App",
-  setup() {},
+  setup() {
+    let name = "owen";
+    let age = 18;
+
+    function SayHello() {
+      alert(`我叫${name},今年${age}歲`);
+    }
+
+    // 返回一個對象
+    return {
+      name,
+      age,
+      SayHello,
+    };
+    // 返回一個渲染函數(了解)
+    // return () => h('h1','owen');
+  },
 };
 </script>
 

1092f737b26093fa452eeaaa6922fb5627b87e55
owen <owen013288@gmail.com>
2022-04-02 21:06:11 +0800

    setup進階 使用props與context參數獲取數據

---
+++ src/components/Vue3setup進階/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Demo @hello="showHelloMsg" msg="您好啊" school="owen">
+    <template v-slot:qwe>
+      <span>OWEN</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+import Demo from "./components/Vue3setup進階/Demo.vue";
+export default {
+  name: "App",
+  components: {
+    Demo,
+  },
+  setup() {
+    function showHelloMsg(value) {
+      alert(`您好啊！您觸發了hello事件，我收到的參數是：${value}`);
+    }
+
+    // 返回一個對象
+    return {
+      showHelloMsg,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Vue3setup進階/Demo.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>一個人的信息</h1>
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年齡：{{ person.age }}</h2>
+  <button @click="test()">測試觸發一下Demo組件的Hello事件</button>
+  <h2>插槽</h2>
+  <slot name="qwe" />
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "App",
+  props: ["msg", "school"],
+  emits: ["hello"],
+  setup(props, context) {
+    console.log("---setup---,props", props);
+    console.log("---setup---,context", context);
+    console.log("---setup---,context.attrs", context.attrs);
+    console.log("---setup---,context.emit", context.emit);
+    console.log("---setup---,context.slots", context.slots);
+    let person = reactive({
+      name: "owen",
+      age: 18,
+    });
+
+    function test() {
+      context.emit("hello", 666);
+    }
+
+    // 返回一個對象
+    return {
+      person,
+      test,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
